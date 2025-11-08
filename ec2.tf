0118544fc78ead5fd9574b10d28d26411b749a4b
Bejohn77 <bejohnroy8@gmail.com>
2025-11-08 16:49:29 +0600

    variables file added and change ec2

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 #key pair
 
 resource "aws_key_pair" "deployer" {
-    key_name = "terra-key-ec2"
-    public_key = file("terra-key-ec2.pub")
-  
+  key_name   = "terra-key-ec2"
+  public_key = file("terra-key-ec2.pub")
+
 }
 
 #vpc and security group
@@ -13,41 +13,41 @@ resource "aws_default_vpc" "default" {
 }
 
 resource "aws_security_group" "my_security" {
-  name = "terra-sg"
+  name        = "terra-sg"
   description = "this is our first terra code"
-  vpc_id = aws_default_vpc.default.id
+  vpc_id      = aws_default_vpc.default.id
 
   # inbound rules 
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "ssh open"
 
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     description = "HTTP open"
   }
 
   ingress {
-    from_port = 8000
-    to_port = 8000
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     description = "flask open"
   }
   # outbound rules
-  egress  {
+  egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
 
   }
 }
@@ -56,11 +56,11 @@ resource "aws_security_group" "my_security" {
 #ec2 instance
 
 resource "aws_instance" "my_instance" {
-  key_name = aws_key_pair.deployer.key_name
-  security_groups = [ aws_security_group.my_security.name ]
-  instance_type = "t3.micro"
-  ami = "ami-0cfde0ea8edd312d4" #ubuntu
-   
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.my_security.name]
+  instance_type   = "t3.micro"
+  ami             = "ami-0cfde0ea8edd312d4" #ubuntu
+
   root_block_device {
     volume_size = 15
     volume_type = "gp3"
@@ -70,5 +70,5 @@ resource "aws_instance" "my_instance" {
     Name = "terraform"
 
   }
-  
+
 }
